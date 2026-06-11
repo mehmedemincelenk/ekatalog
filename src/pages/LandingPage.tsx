024cabe90c12aa5836d48a1cbc1978099dcbf62a
Ekatalog Dev <dev@ekatalog.com>
2026-06-12 00:36:34 +0300

    copy: change landing page subtitle to geçmiş katalog/menü angaryalarına son

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -133,7 +133,7 @@ export default function LandingPage() {
           </h1>
 
           <h2 className="text-lg sm:text-xl font-bold text-stone-900 tracking-tight leading-none relative z-10 pt-2 uppercase">
-            Kağıt Katalog Menü Angaryalarına Son
+            geçmiş katalog/menü angaryalarına son
           </h2>
 
 
